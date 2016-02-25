e5457dc36e404e32fc3eeb7e45fecac5c8be9187
Annie Jiao <anniej@mit.edu>
2016-02-25 14:26:25 +0200

    updates without refreshing

--- public/index.js
+++ public/index.js
@@ -24,35 +24,39 @@
       setup_posters(document.getElementById('ring-1'));
       setup_posters(document.getElementById('ring-2'));
       setup_posters(document.getElementById('ring-3'));
+      getWords();
   }
 
   // call init once the document is fully loaded
   window.addEventListener('load', init, false);
 
 
-  $.get(
-      window.location.origin + '/getWords',
-      function(data) {
-          // console.log(data);
+  function getWords() {
+      $.get(
+          window.location.origin + '/getWords',
+          function(data) {
+              // console.log(data);
 
-          var adj1List = data.adj1.split(",");
-          var adj2List = data.adj2.split(",");
-          var nounList = data.nouns.split(",");
+              var adj1List = data.adj1.split(",");
+              var adj2List = data.adj2.split(",");
+              var nounList = data.nouns.split(",");
 
-          var wordLists = [adj1List, adj2List, nounList];
-          var rings = [$("#ring-1"), $("#ring-2"), $("#ring-3")];
-          for (var j = 0; j < rings.length; j++) {
-              var html = "";
-              for (var i = 0; i < wordLists[j].length; i++) {
-                  html += '<div class="poster" style="-webkit-transform: rotateX(' + i * 30 + 'deg) translateZ(200px); "><p>';
-                  html += wordLists[j][i];
-                  html += '</p></div>';
+              var wordLists = [adj1List, adj2List, nounList];
+              var rings = [$("#ring-1"), $("#ring-2"), $("#ring-3")];
+              for (var j = 0; j < rings.length; j++) {
+                  var html = "";
+                  for (var i = 0; i < wordLists[j].length; i++) {
+                      html += '<div class="poster" style="-webkit-transform: rotateX(' + i * 30 + 'deg) translateZ(200px); "><p>';
+                      html += wordLists[j][i];
+                      html += '</p></div>';
+                  }
+                  rings[j].html(html);
               }
-              rings[j].html(html);
-          }
 
-      }
-  );
+          }
+      );
+  }
+
 
   $(function() {
       $("#wheels").html('<div class="stages" id="stage"> <div class="rotate" id="rotate"> <div id="ring-1" class="ring animated"> </div> </div> </div> <div class="stages" id="stage2"> <div class="rotate" id="rotate"> <div id="ring-2" class="ring animated"> </div> </div> </div> <div class="stages" id="stage3"> <div class="rotate" id="rotate"> <div id="ring-3" class="ring animated"> </div> </div> </div>')
@@ -62,11 +66,14 @@
 
           if (evt.keyCode == 32) {
               // increment space count to see how many wheels should be stopped
+              // if (spaceCount == 3) {
+              //     $("#hideme").show();
+              // }
               if (spaceCount == 3) {
-                  $("#hideme").show();
-              }
-              if (spaceCount == 4) {
-                  location.reload();
+                $(".ring").addClass("animated");
+                  getWords();
+                  spaceCount = 0;
+                  return;
               }
 
               spaceCount += 1;
