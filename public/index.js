4a9bbfcd9ac1d9e8f1d74c76df6db99217bfdd90
Annie Jiao <anniej@mit.edu>
2016-03-26 19:03:36 +0300

    boos IE users

--- public/index.js
+++ public/index.js
@@ -24,7 +24,7 @@
       $.get(
           window.location.origin + '/getWords',
           function(data) {
-             // choose transform notation for browser
+              // choose transform notation for browser
               var isFirefox = typeof InstallTrigger !== 'undefined';
               var transform = "-webkit-transform";
               if (isFirefox) {
@@ -34,7 +34,7 @@
               var wordLists = [data.word1, data.word2, data.word3];
               // populate tagline
               $("#hint").html("<h4>" + data.tagline + "</h4>")
-              
+
               var rings = [$("#ring-1"), $("#ring-2"), $("#ring-3")];
               for (var j = 0; j < rings.length; j++) {
                   var html = "";
@@ -58,40 +58,70 @@
   }
 
   $(window).load(function() {
-      setup_posters(document.getElementById('ring-1'));
-      setup_posters(document.getElementById('ring-2'));
-      setup_posters(document.getElementById('ring-3'));
-      getWords();
-
-      resize();
-
-      $(document).keydown(function(evt) {
-          if (evt.keyCode == 32) {
-              // increment space count to see how many wheels should be stopped
-              if (spaceCount == 3) {
-                  getWords();
-                  $(".ring").addClass("animated");
-                  $(".poster").removeClass("chosen");
-                  spaceCount = 0;
-                  return;
-              }
-
-              spaceCount += 1;
-
-              // handle word reveal of each ring
-              $("#ring-" + spaceCount).removeClass("animated");
-              var children = $("#ring-" + spaceCount).children();
-              var poster = $(children).first();
-
-              // calculate distance stage needs to be from the top for chosen poster to be vertically centered
-              var dtop = $(window).height()/2 - poster.offset().top - poster.height()/2 + $("#stage"+spaceCount).offset().top;
-              var percent = dtop / $(window).height() * 100
-              $("#stage"+spaceCount).css('top', percent + "vh");
-              
-              poster.addClass("chosen");
-
+      /**
+       * detect IE
+       * returns version of IE or false, if browser is not Internet Explorer
+       */
+      function detectIE() {
+          var ua = window.navigator.userAgent;
+          var msie = ua.indexOf('MSIE');
+          if (msie > 0) {
+              // IE 10 or older
+              return true;
           }
-      });
+          var trident = ua.indexOf('Trident/');
+          if (trident > 0) {
+              // IE 11
+              var rv = ua.indexOf('rv:');
+              return true;
+          }
+          // other browser
+          return false;
+      }
+
+      // if IE, boo them
+      if (detectIE()) {
+          $(".isIE").show();
+          $(".container").hide();
+          $(".container").html("");
+      } else {
+          setup_posters(document.getElementById('ring-1'));
+          setup_posters(document.getElementById('ring-2'));
+          setup_posters(document.getElementById('ring-3'));
+          getWords();
+          resize();
+
+          $(document).keydown(function(evt) {
+              if (evt.keyCode == 32) {
+                  // increment space count to see how many wheels should be stopped
+                  if (spaceCount == 3) {
+                      getWords();
+                      $(".ring").addClass("animated");
+                      $(".poster").removeClass("chosen");
+                      spaceCount = 0;
+                      return;
+                  }
+
+                  spaceCount += 1;
+
+                  // handle word reveal of each ring
+                  $("#ring-" + spaceCount).removeClass("animated");
+                  var children = $("#ring-" + spaceCount).children();
+                  var poster = $(children).first();
+
+                  // calculate distance stage needs to be from the top for chosen poster to be vertically centered
+                  var dtop = $(window).height() / 2 - poster.offset().top - poster.height() / 2 + $("#stage" + spaceCount).offset().top;
+                  var percent = dtop / $(window).height() * 100;
+                  $("#stage" + spaceCount).css('top', percent + "vh");
+
+                  poster.addClass("chosen");
+
+              }
+          });
+
+      }
+
+
   });
 
   $(window).resize(resize);
