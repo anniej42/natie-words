a2beb4265787fe5582de7f8d8eb346cdca6747d0
Annie Jiao <anniej@mit.edu>
2016-03-28 15:19:22 +0300

    formatting and sharing title & description

--- public/index.js
+++ public/index.js
@@ -1,142 +1,135 @@
-  const POSTERS_PER_ROW = 12;
-  const RING_RADIUS = 200;
+const POSTERS_PER_ROW = 12;
+const RING_RADIUS = 200;
+var spaceCount = 0;
 
-  var spaceCount = 0;
+function setup_posters(row) {
+    var posterAngle = 360 / POSTERS_PER_ROW;
+    for (var i = 0; i < POSTERS_PER_ROW; i++) {
+        var poster = document.createElement('div');
+        poster.className = 'poster';
 
-  function setup_posters(row) {
-      var posterAngle = 360 / POSTERS_PER_ROW;
-      for (var i = 0; i < POSTERS_PER_ROW; i++) {
-          var poster = document.createElement('div');
-          poster.className = 'poster';
+        // compute and assign the transform for this poster
+        var transform = 'rotateY(' + (posterAngle * i) + 'deg) translateZ(' + RING_RADIUS + 'px)';
+        poster.style.webkitTransform = transform;
+        // setup the number to show inside the poster
+        var content = poster.appendChild(document.createElement('p'));
+        content.textContent = i;
+    }
+}
 
-          // compute and assign the transform for this poster
-          var transform = 'rotateY(' + (posterAngle * i) + 'deg) translateZ(' + RING_RADIUS + 'px)';
-          poster.style.webkitTransform = transform;
-          // setup the number to show inside the poster
-          var content = poster.appendChild(document.createElement('p'));
-          content.textContent = i;
-      }
+// call init once the document is fully loaded
+function getWords() {
+    $.get(
+        window.location.origin + '/getWords',
+        function(data) {
+            // choose transform notation for browser
+            var isFirefox = typeof InstallTrigger !== 'undefined';
+            var transform = "-webkit-transform";
+            if (isFirefox) {
+                transform = "-moz-transform";
+            }
 
-  }
+            var wordLists = [data.word1, data.word2, data.word3];
+            // populate tagline
+            $("#hint").html("<h4>" + data.tagline + "</h4>")
 
-  // call init once the document is fully loaded
-  function getWords() {
-      $.get(
-          window.location.origin + '/getWords',
-          function(data) {
-              // choose transform notation for browser
-              var isFirefox = typeof InstallTrigger !== 'undefined';
-              var transform = "-webkit-transform";
-              if (isFirefox) {
-                  transform = "-moz-transform";
-              }
+            var rings = [$("#ring-1"), $("#ring-2"), $("#ring-3")];
+            for (var j = 0; j < rings.length; j++) {
+                var html = "";
+                for (var i = 0; i < wordLists[j].length; i++) {
+                    html += '<div class="poster" style="' + transform + ': rotateX(' + i * 30 + 'deg) translateZ(200px); "><p>';
+                    html += wordLists[j][i];
+                    html += '</p></div>';
+                }
+                rings[j].html(html);
+            }
+        }
+    );
+}
 
-              var wordLists = [data.word1, data.word2, data.word3];
-              // populate tagline
-              $("#hint").html("<h4>" + data.tagline + "</h4>")
+// calculate and resize upper and lower covers
+function resize() {
+    var hcarve = $(".carve").height();
+    var distanceToTop = ($(window).height() - hcarve) / 2 + 10;
+    $(".wide.blue.top").height(distanceToTop);
+    $(".wide.blue.footer").height(distanceToTop);
+}
 
-              var rings = [$("#ring-1"), $("#ring-2"), $("#ring-3")];
-              for (var j = 0; j < rings.length; j++) {
-                  var html = "";
-                  for (var i = 0; i < wordLists[j].length; i++) {
-                      html += '<div class="poster" style="' + transform + ': rotateX(' + i * 30 + 'deg) translateZ(200px); "><p>';
-                      html += wordLists[j][i];
-                      html += '</p></div>';
-                  }
-                  rings[j].html(html);
-              }
-          }
-      );
-  }
+$(window).load(function() {
+    /**
+     * detect IE
+     * returns version of IE or false, if browser is not Internet Explorer
+     */
+    function detectIE() {
+        var ua = window.navigator.userAgent;
+        var msie = ua.indexOf('MSIE');
+        if (msie > 0) {
+            // IE 10 or older
+            return true;
+        }
+        var trident = ua.indexOf('Trident/');
+        if (trident > 0) {
+            // IE 11
+            var rv = ua.indexOf('rv:');
+            return true;
+        }
+        // other browser
+        return false;
+    }
 
-  // calculate and resize upper and lower covers
-  function resize() {
-      var hcarve = $(".carve").height();
-      var distanceToTop = ($(window).height() - hcarve) / 2 + 10;
-      $(".wide.blue.top").height(distanceToTop);
-      $(".wide.blue.footer").height(distanceToTop);
-  }
+    // if IE, boo them
+    if (detectIE()) {
+        $(".isIE").show();
+        $(".container").hide();
+        $(".container").html("");
+    } else {
+        setup_posters(document.getElementById('ring-1'));
+        setup_posters(document.getElementById('ring-2'));
+        setup_posters(document.getElementById('ring-3'));
+        getWords();
+        resize();
+        $('.circular.button').popup({
+            variation: "mini inverted",
+            position: "top center",
+            prefer: "opposite",
+            inline: "true"
+        });
 
-  $(window).load(function() {
-      /**
-       * detect IE
-       * returns version of IE or false, if browser is not Internet Explorer
-       */
-      function detectIE() {
-          var ua = window.navigator.userAgent;
-          var msie = ua.indexOf('MSIE');
-          if (msie > 0) {
-              // IE 10 or older
-              return true;
-          }
-          var trident = ua.indexOf('Trident/');
-          if (trident > 0) {
-              // IE 11
-              var rv = ua.indexOf('rv:');
-              return true;
-          }
-          // other browser
-          return false;
-      }
+        $("#share").on("click", share_page);
 
+        $(document).keydown(function(evt) {
+            if (evt.keyCode == 32) {
+                // increment space count to see how many wheels should be stopped
+                if (spaceCount == 3) {
+                    getWords();
+                    $(".ring").addClass("animated");
+                    $(".poster").removeClass("chosen");
+                    spaceCount = 0;
+                    return;
+                }
 
+                spaceCount += 1;
 
-      // if IE, boo them
-      if (detectIE()) {
-          $(".isIE").show();
-          $(".container").hide();
-          $(".container").html("");
-      } else {
-          setup_posters(document.getElementById('ring-1'));
-          setup_posters(document.getElementById('ring-2'));
-          setup_posters(document.getElementById('ring-3'));
-          getWords();
-          resize();
-          $('.circular.button').popup({
-              variation: "mini inverted",
-              position: "top center",
-              prefer: "opposite",
-              inline: "true"
-          });
+                // handle word reveal of each ring
+                $("#ring-" + spaceCount).removeClass("animated");
+                var children = $("#ring-" + spaceCount).children();
+                var poster = $(children).first();
 
-          $("#share").on("click", share_page);
+                // calculate distance stage needs to be from the top for chosen poster to be vertically centered
+                var dtop = $(window).height() / 2 - poster.offset().top - poster.height() / 2 + $("#stage" + spaceCount).offset().top;
+                var percent = dtop / $(window).height() * 100;
+                $("#stage" + spaceCount).css('top', percent + "vh");
 
-          $(document).keydown(function(evt) {
-              if (evt.keyCode == 32) {
-                  // increment space count to see how many wheels should be stopped
-                  if (spaceCount == 3) {
-                      getWords();
-                      $(".ring").addClass("animated");
-                      $(".poster").removeClass("chosen");
-                      spaceCount = 0;
-                      return;
-                  }
+                poster.addClass("chosen");
 
-                  spaceCount += 1;
+            }
+        });
+    }
+});
 
-                  // handle word reveal of each ring
-                  $("#ring-" + spaceCount).removeClass("animated");
-                  var children = $("#ring-" + spaceCount).children();
-                  var poster = $(children).first();
+$(window).resize(resize);
 
-                  // calculate distance stage needs to be from the top for chosen poster to be vertically centered
-                  var dtop = $(window).height() / 2 - poster.offset().top - poster.height() / 2 + $("#stage" + spaceCount).offset().top;
-                  var percent = dtop / $(window).height() * 100;
-                  $("#stage" + spaceCount).css('top', percent + "vh");
-
-                  poster.addClass("chosen");
-
-              }
-          });
-
-      }
-
-
-  });
-
-  $(window).resize(resize);
-
-  function share_page() {
-      var fbpopup = window.open("https://www.facebook.com/sharer/sharer.php?u=" + window.location.href, "pop", "width=600, height=400, scrollbars=no");
-      return false;
-  }
+function share_page() {
+    var fbpopup = window.open("https://www.facebook.com/sharer/sharer.php?u=" + window.location.href, "pop", "width=600, height=400, scrollbars=no");
+    return false;
+}
